5e8934f50727cece934ea9b870f77aa56770ce23
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2021-03-04 20:00:38 +0200

    3.21 done.

--- index.js
+++ index.js
@@ -72,26 +72,9 @@ app.delete('/api/persons/:id', (request, response) => {
 
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 
   const body = request.body
-
-  if(!body.name){
-  	return response.status(400).json({ 
-      error: 'Name missing' 
-    })
-  }
-  if(!body.number){
-  	return response.status(400).json({ 
-      error: 'Number missing' 
-    })
-  }
-
-  if(persons.find(candidate => candidate.name === body.name)){
-  	return response.status(400).json({
-  		error: 'Name must be unique'
-  	})
-  }
 
   const person = new Person({
    	name: body.name,
@@ -99,10 +82,11 @@ app.post('/api/persons', (request, response) => {
    	date: Date()
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -116,6 +100,9 @@ const errorHandler = (error, request, response, next) => {
 
   if(error.name === 'CastError'){
     return response.status(400).send({error: 'Malformatted id'})
+  }
+  else if(error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
   }
 
   next(error)
