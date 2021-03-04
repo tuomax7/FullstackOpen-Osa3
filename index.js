e6c92353b9d2836f67eb0eb62aeb59fe680031fb
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2021-03-04 15:58:17 +0200

    3.15 to 3.18 done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 
 const morgan = require('morgan')
@@ -8,11 +9,8 @@ const morgan = require('morgan')
 morgan.token('body', function(req, res) {return JSON.stringify(req.body)});
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
 const cors = require('cors')
 app.use(cors())
-
-app.use(express.static('build'))
 
 //MONGODB
 require('dotenv').config()
@@ -26,8 +24,10 @@ const generateId = () => {
 let persons = []
 
 app.get('/info', (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people. <br>
-  	${Date()}`)
+  Person.find({}).then(persons => {
+    res.send(`<p>Phonebook has info for ${persons.length} people. <br>
+    ${Date()}`)
+  })
 })
 
 app.get('/api/persons', (req, res) => {
@@ -36,17 +36,40 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if(person){
+      response.json(person)
+    }else{
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+
 })
 
 app.post('/api/persons', (request, response) => {
@@ -81,6 +104,24 @@ app.post('/api/persons', (request, response) => {
   })
 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'Unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'Malformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 
--- models/person.js
+++ models/person.js
@@ -23,6 +23,7 @@ personSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
+    delete returnedObject.date
   }
 })
 
